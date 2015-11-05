2882261e5ca5cc0a35a08ce25e06e41f1916f0be
Javier Bravo <javibravo85@gmail.com>
2015-11-05 11:34:18 +0000

    Fixing tests

--- tests/SimplePhpQueue/Unitary/Queue/RedisSourceQueueTest.php
+++ tests/SimplePhpQueue/Unitary/Queue/RedisSourceQueueTest.php
@@ -21,33 +21,33 @@ class RedisQueueTest extends \PHPUnit_Framework_TestCase {
     public function testGetNext() {
         $returnExample = "{string: example}";
         $this->redisClientMock->expects($this->once())->method('brpoplpush')
-            ->with('queue.test', 'queue.test:processing', 20)->willReturn($returnExample);
+            ->with('queue.test', 'queue.test-processing', 20)->willReturn($returnExample);
         $this->assertEquals($returnExample, $this->redisQueue->getNext());
     }
 
     public function testGetNextMaxWaitReached() {
         $this->redisClientMock->expects($this->once())->method('brpoplpush')
-            ->with('queue.test', 'queue.test:processing', 20)->willReturn(null);
+            ->with('queue.test', 'queue.test-processing', 20)->willReturn(null);
         $this->assertTrue(false === $this->redisQueue->getNext());
     }
 
     public function testSuccess() {
         $task = '{data:sample}';
-        $this->redisClientMock->expects($this->once())->method('lrem')->with('queue.test:processing', 1, $task);
+        $this->redisClientMock->expects($this->once())->method('lrem')->with('queue.test-processing', 1, $task);
         $this->redisQueue->successful($task);
     }
 
     public function testFailed() {
         $data = '{data:sample}';
-        $this->redisClientMock->expects($this->once())->method('lpush')->with('queue.test:failed', $data);
-        $this->redisClientMock->expects($this->once())->method('lrem')->with('queue.test:processing', 1, $data);
+        $this->redisClientMock->expects($this->once())->method('lpush')->with('queue.test-failed', $data);
+        $this->redisClientMock->expects($this->once())->method('lrem')->with('queue.test-processing', 1, $data);
         $this->redisQueue->failed($data);
     }
 
     public function testError() {
         $data = '{data:sample}';
-        $this->redisClientMock->expects($this->once())->method('lpush')->with('queue.test:error', $data);
-        $this->redisClientMock->expects($this->once())->method('lrem')->with('queue.test:processing', 1, $data);
+        $this->redisClientMock->expects($this->once())->method('lpush')->with('queue.test-error', $data);
+        $this->redisClientMock->expects($this->once())->method('lrem')->with('queue.test-processing', 1, $data);
         $this->redisQueue->error($data);
     }
 
@@ -58,7 +58,7 @@ class RedisQueueTest extends \PHPUnit_Framework_TestCase {
 
     public function testStopped() {
         $data = '{data:sample}';
-        $this->redisClientMock->expects($this->once())->method('lrem')->with('queue.test:processing', 1, $data);
+        $this->redisClientMock->expects($this->once())->method('lrem')->with('queue.test-processing', 1, $data);
         $this->redisQueue->stopped($data);
     }
 
